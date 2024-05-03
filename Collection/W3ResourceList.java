48bfcb12723c68ed72deac2f3d14070c956e27b7
Bhuvnesh Rana <168338773+Bhuvnesh-Rana@users.noreply.github.com>
2024-05-03 08:34:23 +0000

    W3R ques

--- Collection/W3ResourceList.java
+++ Collection/W3ResourceList.java
@@ -78,18 +78,61 @@ public class W3ResourceList {
     //problem 12 extract portion of a list
     System.out.println("Colours from index 1 to 3: "+colours.subList(1, 4));//to index is not included
     
-    //problem 13 compare two lists *******************************See ans use contains..
-    if (colours.equals(copy)) 
-    System.out.println("Lists are equal.");
-    else
-    System.out.println("Lists are not equal.");   
+    //problem 13 compare two lists 
+    List<String> c = new ArrayList<>();
+    for (String e : colours) {
+        c.add(copy.contains(e)? "yes":"no");
+    }   
+    System.out.println("List colours: "+colours);
+    System.out.println("List copy: "+copy);
+    System.out.println(c);
     
     //problem 14 swap two element in the list
     System.out.println("List before swapping: "+colours);
     Collections.swap(colours, 0, 2);
     System.out.println("List after swapping: "+colours);
-    
 
+    //problem 15 join two lists
+    List<String> join = new ArrayList<>();
+    join.addAll(colours);
+    join.addAll(copy);
+    System.out.println("New joined list is: "+join);
+
+    //problem 16 clone a list to another.
+            //CLONE 
+
+    
+    //problem 17 empty a list.
+    join.removeAll(join);
+    System.out.println("join list: "+join);
+
+    //problem 18 check if list is empty.
+    if (join.isEmpty()) {
+        System.out.println("The list join is empty.");
+    }
+    else
+    System.out.println("List is not empty..");
+
+    //problem 19 trimming the capacity of the list.         ??
+    ArrayList<Integer> q = new ArrayList<>();
+    q.add(1);
+    q.add(2);
+    q.trimToSize();
+    q.add(22);
+    System.out.println(q);
+
+    //problem 20 increase array list size.             ????
+    q.ensureCapacity(2);
+    System.out.println(q);
+
+    //problem 21 relpace element from the list.
+    colours.set(1, "Brown");
+    System.out.println("Colours list after replacing 2nd element: "+colours);
+
+    //problem 22 print all elements of an list.
+    System.out.println("Printing List colours.");
+    for (String f : colours) 
+        System.out.println(f);
 
     }
 }
