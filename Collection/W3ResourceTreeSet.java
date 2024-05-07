1598d3718443e51240f2a04da2b2527b3ad97dd6
Bhuvnesh Rana <168338773+Bhuvnesh-Rana@users.noreply.github.com>
2024-05-07 10:12:54 +0000

    stream probs

--- Collection/W3ResourceTreeSet.java
+++ Collection/W3ResourceTreeSet.java
@@ -3,6 +3,7 @@ package Collection;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class W3ResourceTreeSet {
     
@@ -37,6 +38,50 @@ public class W3ResourceTreeSet {
         //problem 5 get the first and last element of the tree set.
         System.out.println("First element of the tree set "+colours1.first());
         System.out.println("Last element of the tree set: "+colours1.last());
+
+        //problem 6 clone tree set to another tree set
+        @SuppressWarnings("unchecked")
+        TreeSet<String> cloned = (TreeSet<String>) colours1.clone();
+        System.out.println(cloned);
+
+        //problem 7 get number of element in the tree set.
+        System.out.println(colours1.size());
+        
+        //problem 8 compare 2 sets
+
+        //problem 9 numbers less than 7 in a tree set.
+
+        TreeSet<Integer> n = new TreeSet<>();
+        n.add(10);
+        n.add(4);
+        n.add(6);
+        n.add(2);
+
+        Set<Integer> one = n.stream().filter(e -> e<7).collect(Collectors.toSet());
+        System.out.println("Numbers less than 7"+one);
+
+        //problem 10 numbers >= to a specific number.
+        Set<Integer> two = n.stream().filter(e -> e>=4).collect(Collectors.toSet());
+        System.out.println("Numbers >= 4 are: "+two);
+
+        //problem 11 numbers <= to a specific number.
+        Set<Integer> three = n.stream().filter(e -> e<=6).collect(Collectors.toSet());
+        System.out.println("Numbers <= 6 are: "+three);
+
+        //problem 12 numbers > than a specific number.
+        Set<Integer> four = n.stream().filter(e -> e>4).collect(Collectors.toSet());
+        System.out.println("Numbers > 4 are: "+four);
+
+        //problem 13 numbers < than a specific number.
+        Set<Integer> five = n.stream().filter(e -> e<4).collect(Collectors.toSet());
+        System.out.println("Numbers < 4 are: "+five);
+
+        //problem 14 retrieve and remove first element of the tree set.
+
+        //problem 15 retrieve and remove last element of the tree set.
+
+        //problem 16 remove a given element from the tree set.
+
 
 
     }
