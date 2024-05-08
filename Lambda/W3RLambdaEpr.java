6aea2f88b8f380b93c6cdf258cd9a9e1ed4826bf
Bhuvnesh Rana <168338773+Bhuvnesh-Rana@users.noreply.github.com>
2024-05-08 10:31:11 +0000

    updated

--- Lambda/W3RLambdaEpr.java
+++ Lambda/W3RLambdaEpr.java
@@ -1,6 +1,7 @@
 package Lambda;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 interface Sum{
     int sum(int a, int b);
@@ -19,7 +20,25 @@ public static void main(String[] args) {
     int result = summ.sum(2, 2);
     System.out.println(result);
     
-    
+    //problem 2 check if given string is empty. usih lambda func.
+    String a="";
+    System.out.println(a.isEmpty());
+
+    //problem 3 Convert list of string to upper or lowercase.
+    String abc = "we";
+    System.out.println(abc.toUpperCase());
+
+    // List<String> alpha = List.of("random","words","are","entered","here");
+    // alpha.replaceAll(r ->r.toUpperCase());
+    // for (String rr : alpha) {
+    //     System.out.println(rr);
+    // }
+
+    //problem 4 filter out even and odd numbers from a list.
+    List<Integer> numbers = List.of(4,5,1,6,2,3,8,9);
+    List<Integer> rrr = numbers.stream().filter(a1 -> a1%2==0).collect(Collectors.toList());
+    System.out.println(rrr);
+
     
 }
 }
