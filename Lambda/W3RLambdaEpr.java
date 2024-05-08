13eaa3ff1e0afadfdc7331c490e4cb6049dc1115
Bhuvnesh Rana <168338773+Bhuvnesh-Rana@users.noreply.github.com>
2024-05-08 10:00:36 +0000

    pb1

--- Lambda/W3RLambdaEpr.java
+++ Lambda/W3RLambdaEpr.java
@@ -1,9 +1,24 @@
 package Lambda;
 
+import java.util.List;
 
+interface Sum{
+    int sum(int a, int b);
+}
 
 public class W3RLambdaEpr {
 public static void main(String[] args) {
+    
+    //problem 1 find sum of 2 integers using lambda functions.
+    //using stream.
+    // List<Integer> num = List.of(1,2);
+    // int sum = num.stream().mapToInt(Integer::intValue).sum();
+    // System.out.println(sum);
+
+    Sum summ = (a,b)->a+b;
+    int result = summ.sum(2, 2);
+    System.out.println(result);
+    
     
     
 }
