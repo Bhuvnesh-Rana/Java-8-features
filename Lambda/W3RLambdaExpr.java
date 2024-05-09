4efad29200a9e4a9becbb71c4a77db041f59e3ea
Bhuvnesh-Rana <bhuvnesh.rana28@gmail.com>
2024-05-09 15:30:05 +0530

    prob

--- Lambda/W3RLambdaExpr.java
+++ Lambda/W3RLambdaExpr.java
@@ -1,11 +1,17 @@
 package Lambda;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @FunctionalInterface
 interface Sum{
     int sum(int a, int b);
+}
+
+@FunctionalInterface
+interface Factorial{
+    int fact(int a);
 }
 
 public class W3RLambdaExpr {
@@ -21,7 +27,7 @@ public static void main(String[] args) {
     int result = summ.sum(2, 2);
     System.out.println(result);
     
-    //problem 2 check if given string is empty. usih lambda func.
+    //problem 2 check if given string is empty. usig lambda func.
     String a="";
     System.out.println(a.isEmpty());
 
@@ -36,9 +42,45 @@ public static void main(String[] args) {
     // }
 
     //problem 4 filter out even and odd numbers from a list.
-    List<Integer> numbers = List.of(4,5,1,6,2,3,8,9);
+    List<Integer> numbers = List.of(4,5,1,6,2,3,8,9,1,2,4);
     List<Integer> rrr = numbers.stream().filter(a1 -> a1%2==0).collect(Collectors.toList());
     System.out.println(rrr);
+
+    //problem 5 sort list of string in order.
+    List<String> a1 = new ArrayList<>();
+    a1.add("jjj");
+    a1.add("abc");
+    a1.add("qqq");
+    a1.add("bbb");
+    List<String> sortedA1 = a1.stream().sorted().collect(Collectors.toList());
+    System.out.println(sortedA1);
+
+    System.out.println("MTHD-2");
+    a1.sort((b1,b2) -> b1.compareToIgnoreCase(b2));
+    System.out.println(a1);
+
+    //problem 6 find avg of list of integers.
+    double avg = numbers.stream().mapToInt(Integer::intValue).average().orElse(0);
+    System.out.println(avg);
+
+    //problem 7 remove duplicates from list.
+    // List<Integer> dupl = numbers.stream().distinct().collect(Collectors.toList());   // OR
+    List<Integer> dupl = new ArrayList<>();
+    numbers.stream().distinct().forEach(dupl::add);
+    System.out.println(dupl);
+
+    //problem 8 calculate factorial of a given number.
+    int n1 = 7;
+    Factorial f = a11->{
+            int p = 1;
+            for(int i=1;i<=a11;i++)
+                p = p*i;
+            return p;
+        } 
+    ;
+    int result1 = f.fact(n1);
+    System.out.println("Fact: "+result1);
+
 
     
 }
